4e76d88d62fa89e8592d435ddfcf9459e488cc63
Jakson2023 <125270235+Jakson2023@users.noreply.github.com>
2023-07-07 11:28:25 +0000

    Deploying to gh-pages from @ Jakson2023/goit-js-hw-09@18b9df329de3a8860ea85de587a61b46a34a333a 🚀

--- 01-color-switcher.30812ff8.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector(".button[data-start]"),e=document.querySelector(".button[data-stop]");t.addEventListener("click",(function t(){console.log(t)})),e.addEventListener("click",stopClick)}();
-//# sourceMappingURL=01-color-switcher.30812ff8.js.map
--- 01-color-switcher.64b8efeb.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector(".button[data-start]"),e=document.querySelector(".button[data-stop]");t.addEventListener("click",(function t(){console.log(t)})),e.addEventListener("click",stopClick);
-//# sourceMappingURL=01-color-switcher.64b8efeb.js.map
