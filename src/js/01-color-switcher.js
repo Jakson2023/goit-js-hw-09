aee7fb9f60c09e843ae7f20f91483b520ab09023
Jakson2023 <125270235+Jakson2023@users.noreply.github.com>
2023-07-07 15:02:12 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,20 +1,21 @@
 const buttonStart = document.querySelector('button[data-start]');
 const buttonStop = document.querySelector('button[data-stop]');
 let timerId = null;
+buttonStop.disabled = true;
+buttonStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    const randColor = getRandomHexColor();
+    document.body.style.backgroundColor = randColor;
+  }, 1000);
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+});
 
-buttonStart.addEventListener('click', startClick);
-buttonStop.addEventListener('click', stopClick);
-
-function startClick() {
-    timerId = setInterval(()=>{
-        const randColor = getRandomHexColor();
-  document.body.style.backgroundColor = randColor;
-    },1000);
-  
-}
-function stopClick() {
+buttonStop.addEventListener('click', () => {
   clearInterval(timerId);
-}
+  buttonStart.disabled = false;
+  buttonStop.disabled = true;
+});
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
