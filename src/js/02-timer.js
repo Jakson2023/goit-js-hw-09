f13fd0ddc6271b22c025382c2f8c593710d7cca8
Jakson2023 <125270235+Jakson2023@users.noreply.github.com>
2023-07-10 02:00:03 +0300

    2THW

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,15 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+Notiflix.Notify.init({
+  width: '400px',
+  position: 'center-top',
+  distance: '20px',
+  opacity: 1,
+  fontSize: '30px',
+});
+
 const selectors = {
   input: document.querySelector('input[type="text"]'),
   startButton: document.querySelector('button[data-start]'),
@@ -9,75 +19,78 @@ const selectors = {
   seconds: document.querySelector('.field [data-seconds]'),
 };
 
+selectors.startButton.disabled = true;
+selectors.startButton.style.backgroundColor = "red";
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
-    valueSelectedDates(selectedDates[0]);
+    const currentDate = new Date();
+    if (selectedDates[0] <= currentDate) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      selectors.startButton.disabled = false;
+      selectors.startButton.style.backgroundColor = "green";
+    }
   },
 };
 
-flatpickr('#datetime-picker', options);
+const fp = flatpickr('#datetime-picker', options);
 
-let differenceTime = 0;
-let formatDate = null;
 let timerId = null;
-function valueSelectedDates(selectedDates) {
-  const currentDate = new Date();
-  if (selectedDates <= currentDate) {
+
+function onClick() {
+  const selectedDate = fp.selectedDates[0];
+  timerId = setInterval(() =>{
+    const currentDate = new Date();
+    differenceTime = selectedDate - currentDate;
+    differenceTime -= 1000;
     selectors.startButton.disabled = true;
-    return window.alert('Please choose a date in the future');
-  } else {
-    selectors.startButton.disabled = false;
-  }
-differenceTime = selectedDates.getTime() - currentDate;
-formatDate = convertMs(differenceTime);
-console.log(formatDate);
+    selectors.startButton.style.backgroundColor = "red";
+    if (differenceTime < 0) {
+      clearInterval(timerId);
+      selectors.startButton.disabled = false;
+      return;
+    }
+    formatDate(convertMs(differenceTime));
+  },1000)
 }
 
-
+selectors.startButton.addEventListener('click', onClick);
 
 function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
-  }
-  
-  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-  console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-  console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
+  return { days, hours, minutes, seconds };
+}
 
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
-  function onClick(){
-     timerId =  setInterval(timerStart, 1000);
-      function timerStart (){
-      differenceTime -= 1000;
-          
-      }
+function addLeadingZero(value) {
+  return String(value).padStart(2, 0);
+}
 
-  }
-  selectors.startButton.addEventListener ('click', onClick);
-
-
-  console.log(timerStart);
-
-
-
+function formatDate({ days, hours, minutes, seconds }) {
+  selectors.days.textContent = addLeadingZero(days);
+  selectors.hours.textContent = addLeadingZero(hours);
+  selectors.minutes.textContent = addLeadingZero(minutes);
+  selectors.seconds.textContent = addLeadingZero(seconds);
+}
